56294b84d9dcfb6e3ff49877677a9cbb6369e8fd
Ema Tumová <ema.tumova1@gmail.com>
2024-02-18 22:01:12 +0100

    Secondary table cleared from unnecessary comments and finalized.

--- secondary_table.sql
+++ secondary_table.sql
@@ -1,33 +1,13 @@
 /*
-Sekundární tabulka s HDP, Giniho koeficientem a populací evropských států ve stejném období.
-t_ema_tumova_project_SQL_secondary_final
+Sekundární tabulka s HDP, Giniho koeficientem a populací evropských států.
 
-Sloupce budou:
-	ZEMĚ,
-	ROK,
-	HDP,
-	Giniho koeficient,
-	POPULACE
+Struktura dat (sloupce tabulky):
+	zeme,
+	rok (2000 až 2021),
+	hdp (zaokrouhleno na jednotky),
+	gini (= Giniho koeficient),
+	pocet_obyvatel
 */
-
--- Vypíšu si evropské státy (ne zámořská území atd.) z tabulky,
--- abych poté zobrazila pouze data o nich.
-SELECT 
-	DISTINCT country 
-FROM economies;
-
-/*
-Evropské státy: 
-Albania, Andorra, Austria, Belarus, Belgium, Bosnia and Herzegovina, Bulgaria, Croatia, Cyprus, Czech Republic,
-Denmark, Estonia, Finland, France, Germany, Greece, Hungary, Iceland, Ireland, Italy, Kosovo, Latvia,
-Liechtenstein, Lithuania, Luxembourg, Malta, Moldova, Monaco, Montenegro, Netherlands, North Macedonia,
-Norway, Poland, Portugal, Romania, San Marino, Serbia, Slovakia, Slovenia, Spain, Sweden, Switzerland,
-Ukraine, United Kingdom 
-*/
-
--- Nejširší období s dostupnými daty, která budu primárně zkoumat (mzdy a ceny), jsou roky 2000 až 2021 (mzdy).
--- Omezím tedy data v sekundární tabulce právě na roky 2000 až 2021.
--- HDP pro přeheldnost zaokrouhlím na jednotky.
 
 CREATE TABLE IF NOT EXISTS t_ema_tumova_project_SQL_secondary_final
 AS
